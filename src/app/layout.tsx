7721196e89d68dafea0d94786347b81027f2f83f
Timothy Achonu <achonutimothy@gmail.com>
2025-09-12 12:37:41 +0100

    add local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,43 @@
 import type { Metadata } from "next";
-import { Open_Sans, Raleway } from "next/font/google";
+import {
+  Open_Sans,
+  // Raleway
+} from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "./providers";
 import { QueryProvider } from "@/context";
+import localFont from "next/font/local";
+
+export const raleway = localFont({
+  src: [
+    {
+      path: "../../public/fonts/raleway/Raleway-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/raleway/Raleway-SemiBold.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/raleway/Raleway-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-raleway", // optional if you want a CSS variable
+  display: "swap",
+});
 
 const OpenSan = Open_Sans({ weight: "400", subsets: ["latin"] });
-export const raleway = Raleway({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--raleway-font",
-});
+// export const raleway = Raleway({
+//   subsets: ["latin"],
+//   display: "swap",
+//   variable: "--raleway-font",
+// });
 
 export const metadata: Metadata = {
   title: "ThoughtTrace",
